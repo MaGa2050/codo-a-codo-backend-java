e48adbe2caafec18baa267d16585b84380cf2b06
MaGa2050 <alebalcarce@gmail.com>
2022-06-16 17:39:02 -0300

    dev creado

---
+++ src/test/java/codo/a/codo/For.java
@@ -0,0 +1,13 @@
+package codo.a.codo;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("INgrese Numero" );
+		for(int i=0;<=10;i++ )
+	}
+
+}
+// for (inicializa variable;)
---
+++ src/test/java/codo/a/codo/Switch.java
@@ -0,0 +1,22 @@
+package codo.a.codo;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nota = 7;
+		
+		switch (nota) {
+		case 7:
+			System.out.println("aprobado");
+			brek;
+		case 10:
+			System.out.println("excelente");
+			break;
+		default:
+			System.out.println("seguí participando");
+			break;
+		}
+		}
+
+}
